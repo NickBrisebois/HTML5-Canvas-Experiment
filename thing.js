f7e97f674fda0456a96a5e0dfe05055fad193e1a
NickBrisebois <nickbrisebois12@gmail.com>
2016-06-12 22:32:01 -0400

    No more images, no more testDot, and now all dots connect to each other within a certain radius

--- thing.js
+++ thing.js
@@ -2,7 +2,6 @@ var canvas = document.getElementById("thing");
 var ctx = canvas.getContext("2d");
 
 var timer = setInterval(update, 1/100);
-var dick = new Image();
 var toDraw = [];
 var dots = [];
 
@@ -19,18 +18,8 @@ for(var i=0; i<100; i++){
 	dots[i] = new Dot(Math.random()*canvas.width, Math.random()*canvas.height, "#000", 3, 3);
 }
 
-var testDot = new Dot(100, 10, "#84AB50", Math.floor(canvas.width*Math.random()), Math.floor(canvas.height*Math.random())); 
-
 function update() {
-	var testDotMovement = calculateMovement(testDot);
-	testDot.x += testDotMovement[0];
-	testDot.y -= testDotMovement[1];
-
-	if(distance(testDot.x, testDot.y, testDot.movingToX, testDot.movingToY) <= 1){
-		testDot.movingToX = Math.floor(canvas.width*Math.random());
-		testDot.movingToY = Math.floor(canvas.height*Math.random());
-	}
-
+	
 	for(var i=0; i<dots.length; i++){
 		if(distance(dots[i].x, dots[i].y, dots[i].movingToX, dots[i].movingToY) <= 1){
 			dots[i].movingToX = Math.floor(canvas.width*Math.random());
@@ -44,28 +33,34 @@ function update() {
 	draw();
 }
 
+
 function draw(){
 	canvas.width = canvas.width;
 	for(var i=0; i<dots.length; i++){
 		var currentDot = dots[i];
-		ctx.drawImage(currentDot.img, currentDot.x, currentDot.y);
-		currentDot.img.src = './dick.png';
-		var difference = distance(currentDot.x, currentDot.y, testDot.x, testDot.y);
-		if(difference < 100) {
-			ctx.beginPath();
-			ctx.moveTo(testDot.x, testDot.y);
-			ctx.lineTo(currentDot.x, currentDot.y);
-			ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(currentDot.x, currentDot.y, 2, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'black';
+		ctx.fill();
+		ctx.stroke();
+		for(var ii=0; ii<dots.length; ii++){
+			var difference = distance(currentDot.x, currentDot.y, dots[ii].x, dots[ii].y);
+			if(difference < 100) {
+				ctx.beginPath();
+				ctx.moveTo(dots[ii].x, dots[ii].y);
+				ctx.lineTo(currentDot.x, currentDot.y);
+				ctx.stroke();
+			}
 		}
 	}
-	ctx.fillStyle = testDot.colour;
-	ctx.fillRect(testDot.x, testDot.y, 30, 30);
 }
 
+
 function distance(dot1X, dot1Y, dot2X, dot2Y){
-	//Pythagorean theorem to find difference between dot[i] and testDot
+	//Pythagorean theorem to find distance between the given dots
 	return Math.sqrt( Math.pow(( dot1X - dot2X ), 2) + Math.pow( ( dot1Y - dot2Y ), 2)); 
 }
+
 
 function calculateMovement(dot) {
 	angle = Math.atan2(dot.y - dot.movingToY, dot.x - dot.movingToX) * (180/Math.PI);
